ba207d2753c91810ba95793f2e4c386d49b3ea4e
JSzeronine <kineage18@naver.com>
2026-09-18 14:44:47 +0900

    Create new.js

---
+++ data/story/2026/09/09-16/new.js
@@ -0,0 +1,82 @@
+/*
+The Voice Under My Bed
+It was late at night, and I was lying in my bed.
+늦은 밤이었고, 나는 침대에 누워 있었어요.
+
+My parents were sleeping in the next room.
+부모님은 옆방에서 자고 계셨어요.
+
+I was reading a book when I heard a small voice.
+책을 읽고 있는데 작은 목소리가 들렸어요.
+
+“Are you still awake?”
+“너 아직 안 자?”
+
+I looked around my room, but no one was there.
+나는 방을 둘러봤지만 아무도 없었어요.
+
+Then I heard the voice again.
+그러자 다시 그 목소리가 들렸어요.
+
+“I am under your bed.”
+“나 네 침대 밑에 있어.”
+
+I stopped breathing for a moment.
+나는 순간 숨을 멈췄어요.
+
+I wanted to call my mom, but I was too scared to move.
+엄마를 부르고 싶었지만 너무 무서워서 움직일 수가 없었어요.
+
+Then something touched my foot.
+그때 무언가가 내 발을 만졌어요.
+
+I quickly pulled my feet onto the bed.
+나는 재빨리 발을 침대 위로 올렸어요.
+
+“Please don't look under the bed,” the voice said.
+“제발 침대 밑을 보지 마.” 목소리가 말했어요.
+
+That made me even more curious.
+그 말을 들으니 오히려 더 궁금해졌어요.
+
+I slowly got out of bed and looked down.
+나는 천천히 침대에서 내려와 아래를 바라봤어요.
+
+At first, I saw only darkness.
+처음에는 어둠밖에 보이지 않았어요.
+
+Then I saw two small eyes looking back at me.
+그러다 나를 바라보는 작은 눈 두 개가 보였어요.
+
+I screamed and ran to my parents' room.
+나는 소리를 지르며 부모님 방으로 달려갔어요.
+
+My dad turned on the light and came with me.
+아빠가 불을 켜고 나와 함께 갔어요.
+
+We looked under the bed together.
+우리는 함께 침대 밑을 살펴봤어요.
+
+There was nothing there.
+그곳에는 아무것도 없었어요.
+
+My dad smiled and told me it was just a dream.
+아빠는 미소를 지으며 그냥 꿈이었을 거라고 말했어요.
+
+I went back to bed and tried to sleep.
+나는 다시 침대로 가서 잠을 자려고 했어요.
+
+Then I heard a voice right next to my ear.
+그때 바로 내 귀 옆에서 목소리가 들렸어요.
+
+“I told you not to look under the bed.”
+“침대 밑을 보지 말라고 했잖아.”
+
+I slowly turned around, and I saw myself standing beside my bed.
+나는 천천히 뒤를 돌아봤고, 침대 옆에 서 있는 또 다른 나를 봤어요.
+
+
+
+
+
+*/
